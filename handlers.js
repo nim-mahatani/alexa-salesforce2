f6a6f86ffe285802f4b12417160e7700907af394
nim-mahatani <nim.mahatani@gmail.com>
2017-11-22 16:17:46 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -112,7 +112,7 @@ exports.newLead = (slots, session, response) => {
            console.log(session.attributes.leadfirstname);
            console.log(session.attributes.leadlastname);
         console.log(session.attributes.companyname);
-           salesforce.createLead({firstname:session.attributes.leadfirstname, lastname:session.attributes.leadlastname, company:session.attributes.companyname})
+           salesforce.createLead({firstname: session.attributes.leadfirstname, lastname: session.attributes.leadlastname, company: session.attributes.companyname})
                 .then(leads => {
                         let text = "Lead has been created ";
                              response.say(text);      
